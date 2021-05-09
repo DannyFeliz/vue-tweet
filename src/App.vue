4795d98ca5f3e00e45e862aef35780f40859698a
Danny Feliz <dannyfeliz08@gmail.com>
2021-05-09 15:10:11 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <TweetEmbed
+    class="my-custom-class"
+    tweet-id="20"
+    :options="{ lang: 'es' }"
+    :onTweetLoadError="onTweetLoadError"
+    :onTweetLoadSuccess="onTweetLoadSuccess"
+  >
+    <template v-slot:loading>
+      <span>Loading...</span>
+    </template>
+
+    <template v-slot:error>
+      <span>Sorry, that tweet doesn’t exist!</span>
+    </template>
+  </TweetEmbed>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import TweetEmbed from './components/TweetEmbed.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    TweetEmbed
+  },
+  methods: {
+    onTweetLoadSuccess(element: HTMLElement) {
+      console.log(element)
+    },
+    onTweetLoadError() {
+      console.log("YR")
+    }
+  }
+})
+</script>
