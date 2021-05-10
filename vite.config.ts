5ee70b2ffd8b138d0ba07cfe0c2d8be0e3d9674f
Danny Feliz <dannyfeliz08@gmail.com>
2021-05-10 03:34:10 -0400

    Add lib config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/components/vue-tweet.vue"),
+      name: "vue-tweet",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn"t be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
