bb57eb30ec82ae342ed497719382337a63c078f6
Danny Feliz <dannyfeliz08@gmail.com>
2024-03-29 02:11:17 -0400

    Render error in template

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,37 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import VueTweet from "./vue-tweet.vue";
 
 function onTweetLoadSuccess(embedNode: HTMLDivElement) {
   console.log("onTweetLoadSuccess callback", embedNode);
 }
+
+const tweetError = ref<Error | null>(null);
 function onTweetLoadError(error: Error) {
+  tweetError.value = error;
   console.log("onTweetLoadError callback", error);
 }
 </script>
 
 <template>
     <vue-tweet
-    tweet-url="https://twitter.com/DannyFeliz08/status/15302400858070548448"
-    cards="visible"
-    conversation="all"
-    lang="en"
-    theme="light"
-    align="left"
-    :width="400"
-    :dnt="false"
-    @tweet-load-error="onTweetLoadError"
-    @tweet-load-success="onTweetLoadSuccess"
-  >
-    <template v-slot:loading>
-      <span>Loading...</span>
-    </template>
+      tweet-url="https://twitter.com/vuejs/status/1753678159067881809"
+      cards="visible"
+      conversation="all"
+      lang="en"
+      theme="light"
+      align="left"
+      :width="400"
+      :dnt="false"
+      @tweet-load-error="onTweetLoadError"
+      @tweet-load-success="onTweetLoadSuccess"
+    >
+      <template v-slot:loading>
+        <span>Loading...</span>
+      </template>
 
-    <template v-slot:error>
-      <span>Sorry, that tweet doesn't exist!</span>
-    </template>
-  </vue-tweet>
+      <template v-slot:error>
+        <span>{{ tweetError?.message }}</span>
+      </template>
+    </vue-tweet>
 </template>
